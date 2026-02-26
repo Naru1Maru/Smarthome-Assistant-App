7935214717b2ebc584df30b61bf425d6b47d016b
Naru1Maru <konokhova.em@yandex.ru>
2026-02-26 14:04:52 +0300

    chore: remove obsolete debug scripts

--- smarthome_core_v1_haexec_brightnessfix/patch_parser6.py
+++
@@ -1,6 +0,0 @@
-﻿# -*- coding: utf-8 -*-
-from pathlib import Path
-path = Path(r'C:\\Users\\narumaru\\workspace\\Diplom\\smarthome_core_v1_haexec_brightnessfix\\smarthome_core\\parser_llm.py')
-text = path.read_text(encoding='utf-8')
-text = text.replace('                return parse_light_command_v1(\n                    text,\n                    context=context,\n                    device_registry=device_registry,\n                    area_synonyms=area_synonyms,\n                    colors=colors,\n                    modifiers=modifiers,\n                )', '                return _run_rule_parser()', 3)
-path.write_text(text, encoding='utf-8')
--- smarthome_core_v1_haexec_brightnessfix/which_module.py
+++
@@ -1,3 +0,0 @@
-﻿# -*- coding: utf-8 -*-
-import smarthome_core.parser_llm as pl
-print(pl.__file__)
